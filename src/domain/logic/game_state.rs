a5489f971134c9e9d82d26d1e718db574124ce12
abaars <abaars@sogyo.nl>
2025-01-06 17:23:39 +0100

    Added GameState struct Started work on mine table generation

--- src/domain/logic/game_state.rs
+++ src/domain/logic/game_state.rs
@@ -1,9 +1,30 @@
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 use rand::{distributions::Uniform, Rng};
-use itertools::Itertools;
+use itertools::*;
 
-struct GameState {
+// Building up the board
+pub fn generate_seed() -> u64 {
+    let mut rng: ThreadRng = rand::thread_rng();
+    rng.gen()
+}
+
+pub fn generate_mine_indices(number_of_squares: u32, seed: u64) -> Vec<u32> {
+    let mut rng_seeded: StdRng = StdRng::seed_from_u64(seed);
+    let range = Uniform::from(0..number_of_squares);
+    let number_to_generate: u32 = number_of_squares * 100;
+    let mine_density: u32 = number_of_squares / 5;
+    let values: Vec<u32> = range.sample_iter(&mut rng_seeded).take(number_to_generate.try_into().unwrap()).unique().take(mine_density.try_into().unwrap()).sorted().collect();
+    return values;
+}
+
+fn vector_to_matrix(vector: Vec<T>) -> Vec<Vec<T>> {
+
+}
+
+
+
+pub struct GameState {
     mines: Vec<Vec<bool>>,
     vicinity: Vec<Vec<u8>>,
     revealed: Vec<Vec<bool>>,
@@ -20,25 +41,22 @@ impl GameState {
         }
     }
 
-    // Building up the board
-    fn generate_seed() -> u64 {
-        let mut rng: ThreadRng = rand::thread_rng();
-        rng.gen()
+    fn generate_mines(board_size: u32, seed: u64) -> Vec<Vec<bool>> {
+        let number_of_squares: u32 = board_size^2;
+        let mine_indices: Vec<u32> = generate_mine_indices(number_of_squares, seed);
+        let board: Vec<Vec<bool>> = vector_to_matrix(
+            (0..number_of_squares)
+            .map(|x: u32| mine_indices.contains(&x))
+            .collect()
+        );
+        return board;
     }
-
-    fn generate_mine_indices(number: u8, seed: u64) -> Vec<u8> {
-        let mut rng_seeded: StdRng = StdRng::seed_from_u64(seed);
-        let range = Uniform::from(0..number);
-        let values: Vec<u8> = range.sample_iter(&mut rng_seeded).take(1000).unique().take(40).sorted().collect();
-        return values;
-    }
-
-
 }
 
 
 
 fn main() {
-    let seed: u64 = generate_seed();
-    println!("{:?}", generate_mine_indices(255, seed))
+    let seed: u64 = 1234567890;
+    println!("{:?}", generate_mine_indices(255, seed));
+    println!("{}", generate_mine_indices(255, seed).len());
 }
--- tests/test_game_state.rs
+++ tests/test_game_state.rs
@@ -1,30 +1,32 @@
 #[cfg(test)]
 mod tests {
-    use rusty_minesweeper::domain::logic::game_state::{self};
+    use rusty_minesweeper::domain::logic::game_state::{self, generate_mine_indices};
 
-    //[2, 6, 9, 15, 25, 34, 41, 43, 50, 52, 67, 78, 84, 85, 89, 90, 97, 102, 104, 111, 119, 122, 124, 125, 129, 
-    // 138, 143, 160, 165, 166, 175, 183, 191, 217, 231, 234, 240, 248, 249, 254]
+    // [2, 6, 7, 9, 15, 18, 21, 25, 34, 41, 43, 50, 52, 67, 70, 78, 84, 85, 89, 90, 97, 102, 104, 111, 114, 119, 122, 124, 125, 
+    // 129, 131, 138, 143, 160, 165, 166, 168, 175, 183, 190, 191, 192, 217, 231, 234, 237, 240, 243, 248, 249, 254]
 
     #[test]
     fn assert_board_generation_is_consistent_with_seed() -> () {
         let test_seed: u64 = 1234567890;
-        let test_field: Vec<Vec<bool>> = [
-            [false, false, true, false, false, false, true, false, false, true, false, false, false, false, false, true],
-            [false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false],
-            [false, false, true, false, false, false, false, false, false, true, false, true, false, false, false, false],
-            [false, false, true, false, true, false, false, false, false, false, false, false, false, false, false, false],
-            [false, false, false, true, false, false, false, false, false, false, false, false, false, false, true, false],
-            [false, false, false, false, true, true, false, false, false, true, true, false, false, false, false, false],
-            [false, true, false, false, false, false, true, false, true, false, false, false, false, false, false, true],
-            [false, false, false, false, false, false, false, true, false, false, true, false, true, true, false, false],
-            [false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true],
-            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-            [true, false, false, false, false, true, true, false, false, false, false, false, false, false, false, true],
-            [false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, true],
-            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-            [false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false],
-            [false, false, false, false, false, false, false, true, false, false, true, false, false, false, false, false],
-            [true, false, false, false, false, false, false, false, true, true, false, false, false, false, true, false]
-        ]
+        // let test_field: Vec<Vec<bool>> = vec![
+        //     vec![false, false, true, false, false, false, true, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        //     vec![false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+        // ];
+        println!("{:?}", generate_mine_indices(255, test_seed));
+        println!("{}", generate_mine_indices(255, test_seed).len());
     }
 }
