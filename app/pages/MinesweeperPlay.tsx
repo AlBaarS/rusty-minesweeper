5575eb41d1f3d35340a280516d178ecdd421c958
abaars <abaars@sogyo.nl>
2025-01-16 11:59:40 +0100

    Added page structure with squares

--- app/pages/MinesweeperPlay.tsx
+++ app/pages/MinesweeperPlay.tsx
@@ -1,5 +1,70 @@
 export const MinesweeperPlay = () => {
-    return(<div>It works yo!</div>)
+
+    function FlagsLeft() {
+        return(<div></div>)
+    }
+
+    function Timer() {
+        return(<div></div>)
+    }
+
+    function Row({x}) {
+        return(
+            <div className="grid grid-flow-row">
+                <Square x={x} y={0} />
+                <Square x={x} y={1} />
+                <Square x={x} y={2} />
+                <Square x={x} y={3} />
+                <Square x={x} y={4} />
+                <Square x={x} y={5} />
+                <Square x={x} y={6} />
+                <Square x={x} y={7} />
+                <Square x={x} y={8} />
+                <Square x={x} y={9} />
+                <Square x={x} y={10} />
+                <Square x={x} y={11} />
+                <Square x={x} y={12} />
+                <Square x={x} y={13} />
+                <Square x={x} y={14} />
+                <Square x={x} y={15} />
+            </div>
+        )
+    }
+
+    function Square({x, y}) {
+        return(
+            <div>
+                x={x}, y={y}
+            </div>
+        )
+    }
+
+
+    return(
+        <div>
+            <div>
+                <FlagsLeft /> <Timer />
+                <div className="grid grid-flow-col">
+                    <Row x={0}/>
+                    <Row x={1}/>
+                    <Row x={2}/>
+                    <Row x={3}/>
+                    <Row x={4}/>
+                    <Row x={5}/>
+                    <Row x={6}/>
+                    <Row x={7}/>
+                    <Row x={8}/>
+                    <Row x={9}/>
+                    <Row x={10}/>
+                    <Row x={11}/>
+                    <Row x={12}/>
+                    <Row x={13}/>
+                    <Row x={14}/>
+                    <Row x={15}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default MinesweeperPlay;
