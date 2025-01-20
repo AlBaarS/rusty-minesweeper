8421d814e0820031d9d32681fa19df5d5a028d4f
abaars <abaars@sogyo.nl>
2025-01-20 10:00:26 +0100

    Added groundwork for the database

--- db/rusty_minesweeper_db.sql
+++ db/rusty_minesweeper_db.sql
@@ -17,3 +17,59 @@ CREATE TABLE IF NOT EXISTS Games(
     FOREIGN KEY(user) REFERENCES Users(user)
 );
 
+
+
+-- Store a user in the user database
+DROP PROCEDURE IF EXISTS CreateUser;
+
+DELIMITER //
+
+CREATE PROCEDURE CreateUser(
+    user_name TINYTEXT
+)
+
+BEGIN
+    INSERT INTO Users(user)
+    VALUES(user_name);
+    
+END//
+
+DELIMITER ;
+
+
+-- Store a game under a user
+DROP PROCEDURE IF EXISTS StoreGame;
+
+DELIMITER //
+
+CREATE PROCEDURE StoreGame(
+    user_name TINYTEXT,
+    game_state JSON
+)
+
+BEGIN
+    INSERT INTO Games(user, game)
+    VALUES(user_name, game_state)
+    ON DUPLICATE KEY UPDATE
+        game = game_state;
+
+END//
+
+DELIMITER ;
+
+
+-- Fetch a game from the database if it exists
+DROP PROCEDURE IF EXISTS FetchGame;
+
+DELIMITER //
+
+CREATE PROCEDURE FetchGame(
+    user_name TINYTEXT
+)
+
+BEGIN
+    SELECT game FROM Games WHERE user = user_name;
+
+END//
+
+DELIMITER ;
