ad9bbb41c0ce3b68cb1ae4e99607ef9b260bd95b
abaars <abaars@sogyo.nl>
2025-02-03 13:26:15 +0100

    Updated README and .init.sh

--- .init.sh
+++ .init.sh
@@ -1,5 +1,5 @@
 sudo systemctl start mongod
 
-(trap 'kill 0' SIGINT; (npm run dev) & (cd backend/ ; cargo run))
+(trap 'kill 0' SIGINT; (npm run build) & (cd backend/ ; cargo build --release))
 
 sudo systemctl stop mongod
